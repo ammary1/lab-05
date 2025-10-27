5d9a694ea8a1615cd44f4a8863a9cc0aba615569
ammary1 <ammarnt56@gmail.com>
2025-10-27 17:43:25 +0300

    Add player data to data.js

---
+++ data.js
@@ -0,0 +1,10 @@
+const players = [
+  { name: "LeBron James", team: "LAL", pos: "F", ppg: 25.7, apg: 8.3, rpg: 7.3 },
+  { name: "Stephen Curry", team: "GSW", pos: "G", ppg: 26.4, apg: 5.1, rpg: 4.5 },
+  { name: "Nikola Jokic", team: "DEN", pos: "C", ppg: 26.1, apg: 9.0, rpg: 12.1 },
+  { name: "Jayson Tatum", team: "BOS", pos: "F", ppg: 26.9, apg: 4.4, rpg: 8.1 },
+  { name: "Shai Gilgeous-Alexander", team: "OKC", pos: "G", ppg: 30.1, apg: 6.2, rpg: 5.5 },
+  { name: "Giannis Antetokounmpo", team: "MIL", pos: "F", ppg: 28.4, apg: 5.7, rpg: 11.2 },
+  { name: "Luka Doncic", team: "DAL", pos: "G", ppg: 32.5, apg: 8.2, rpg: 9.1 },
+  { name: "Kevin Durant", team: "PHX", pos: "F", ppg: 29.8, apg: 6.0, rpg: 7.4 }
+];
